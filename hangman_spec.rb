3f0a25e30d8015fd31cfb3bfaebd63ea898098fa
Alex Fox <alexdysonfox@gmail.com>
2014-06-17 15:37:06 -0400

    Testing Game and Guess

--- hangman_spec.rb
+++ hangman_spec.rb
@@ -12,18 +12,20 @@ end
 
 class Guess
   def get_guess
-    puts "guess the letters in the word..."
+    print "Guess the letters in the word..."
     @guess = STDIN.gets.chomp
   end
 end
 
 class Game
-  attr_accessor :word
+  attr_accessor :word, :letters
 
   def initialize
     print "Welcome to Fox Hangman"
     @word = WordFile.select_word
+    @letters = @word.split ""
   end
+
 end
 
 describe 'game' do
@@ -35,5 +37,33 @@ describe 'game' do
     game = Game.new
 
     expect(game.word).to_not be_nil
+  end
+
+  it 'selects letters from word' do
+    game = Game.new
+
+    expect(game.letters.count).to eq(game.word.size)
+  end
+end
+
+describe 'guess' do
+  it 'gets guess from player' do
+    guess = Guess.new
+
+    expect(guess.get_guess).to_not be_nil
+  end
+
+  it 'player gets correct guess' do
+    guess = Guess.new
+    letters = WordFile.select_word.split ""
+
+    expect(letters).to include(guess.get_guess)
+  end
+
+  it 'player gets incorrect guess' do
+    guess = Guess.new
+    letters = WordFile.select_word.split ""
+
+    expect(letters).to_not include(guess.get_guess)
   end
 end
